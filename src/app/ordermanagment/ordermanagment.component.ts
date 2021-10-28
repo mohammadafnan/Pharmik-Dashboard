27b800ea72d0320f1edce7e90164cd23c1142fcd
mohammadafnan29 <mohammadafnan29@gmail.com>
2021-10-29 03:56:21 +0500

    order management UI works

--- src/app/ordermanagment/ordermanagment.component.ts
+++ src/app/ordermanagment/ordermanagment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Editor } from 'ngx-editor';
 
 @Component({
   selector: 'app-ordermanagment',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ordermanagment.component.scss']
 })
 export class OrdermanagmentComponent implements OnInit {
+  editor: Editor;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.editor = new Editor();
+
   }
+  // make sure to destory the editor
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+
+  openModal( exampleModalContent ) {
+    this.modalService.open( exampleModalContent, { size : 'lg' } );
+  }
+
+  name = 'Angular 4';
+  url :any;
+  onSelectFile(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+    }
+  }
+  public delete(){
+    this.url = '';
+  }
+
 
 }
