965dbeb0e90b14dc652e0801b5b60732ca252e04
mohammadafnan29 <mohammadafnan29@gmail.com>
2021-11-13 00:37:17 +0500

    ui for dahboard

--- src/app/user-pages/login/login.component.ts
+++ src/app/user-pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup, FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
@@ -9,33 +9,37 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  public formdata;
+  public formdata:FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,  public form: FormBuilder,) { }
 
   ngOnInit() {
     this.formdata = new FormGroup({
-      // username: new FormControl("", Validators.compose([
-      //   Validators.required,
-      //   Validators.minLength(10)
-      // ])),
-      // password: new FormControl("", this.passwordvalidation)
+      username: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(10)
+      ])),
+      password: new FormControl("", this.passwordvalidation)
     })
 
 
   }
-  //   passwordvalidation(formcontrol) {
-  //     if (formcontrol.value.length > 8) {
-  //        return {"passwd" : true};
-  //     }
+    passwordvalidation(formcontrol) {
+      if (formcontrol.value.length > 8) {
+         return {"passwd" : true};
+      }
 
-  //  }
+   }
   onClickSubmit(data) {
-    // alert("123");
+    // alert("1");
 
     console.log(data.username);
+    // alert("2");
+
     if (data.username == "admin" && data.password == "123") {
       alert("Login Successful");
+      // alert("3");
+
       this.router.navigate(['dashboard'])
     } else {
       alert("Invalid Login");
